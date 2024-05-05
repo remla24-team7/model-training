283b744aceff29b9d39aae0e433c31a9b5c72992
kevin <kevintran@live.nl>
2024-05-05 22:01:13 +0200

    Added the ROC curve to model_evaluate, also included the confusion matrix and roc curve to the output of the evaluate_model stage

--- src/models/model_evaluate.py
+++ src/models/model_evaluate.py
@@ -3,11 +3,12 @@ Module for evaluating the neural network model trained.
 """
 
 from keras.src.saving import load_model
-from sklearn.metrics import classification_report, confusion_matrix,accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix,accuracy_score, roc_curve, roc_auc_score
 from joblib import load
 import numpy as np
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 model = load_model('outputs/model.h5')
 
@@ -36,3 +37,19 @@ print('Accuracy:',accuracy_score(y_test,y_pred_binary))
 ax = sns.heatmap(confusion_mat,annot=True)
 
 ax.figure.savefig('outputs/confusion_matrix.png')
+
+# Calculate the false positive rate, true positive rate, and thresholds
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_binary)
+
+# Calculate the area under the ROC curve
+auc = roc_auc_score(y_test, y_pred_binary)
+
+# Plot the ROC curve
+plt.figure()
+plt.plot(fpr, tpr)
+plt.plot([0, 1], [0, 1], 'k--')  # Plot the diagonal line
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(['ROC curve (AUC = {:.2f})'.format(auc)])
+plt.savefig('outputs/roc_curve.png')
