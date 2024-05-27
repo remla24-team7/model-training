13bbd00bfbe98481cd0ea4339451aebc4d9ac7d0
avilakathara <59567989+avilakathara@users.noreply.github.com>
2024-05-27 05:06:15 +0200

    Add tests and github workflow action

---
+++ tests/test_model_evaluate.py
@@ -0,0 +1,13 @@
+# test_evaluate_model.py
+import pytest
+from src.models.model_evaluate import evaluate_model
+
+def test_evaluate_model():
+    model_path = '../outputs/model.h5'
+    x_test_path = '../outputs/x_test.joblib'
+    y_test_path = '../outputs/y_test.joblib'
+    report, confusion_mat, accuracy, auc = evaluate_model(model_path, x_test_path, y_test_path, True)
+
+    assert confusion_mat.size > 0, "Confusion matrix should not be empty"
+    assert 0 <= accuracy <= 1, "Accuracy should be between 0 and 1"
+    assert 0 <= auc <= 1, "AUC should be between 0 and 1"
---
+++ tests/test_model_train.py
@@ -0,0 +1,47 @@
+# test_train_model.py
+import pytest
+from joblib import load
+from src.models.model_train import load_data, load_data_test, train_model, build_model
+
+
+def test_load_data():
+    x_train, y_train, x_val, y_val, char_index = load_data_test()
+    assert x_train is not None, "x_train should not be None"
+    assert y_train is not None, "y_train should not be None"
+    assert x_val is not None, "x_val should not be None"
+    assert y_val is not None, "y_val should not be None"
+    assert char_index is not None, "char_index should not be None"
+
+
+def test_build_model():
+    char_index = load('../outputs/char_index.joblib')
+    params = {'loss_function': 'binary_crossentropy',
+              'optimizer': 'adam',
+              'sequence_length': 200,
+              'batch_train': 5000,
+              'batch_test': 5000,
+              'categories': ['phishing', 'legitimate'],
+              'char_index': None,
+              'epoch': 30,
+              'embedding_dimension': 50,
+              'dataset_dir': "../dataset/small_dataset/"
+              }
+    voc_size = len(char_index.keys())
+    model = build_model(voc_size, params['categories'], params['embedding_dimension'])
+    assert model is not None, "Model should be initialized"
+
+
+def test_train_model():
+    x_train, y_train, x_val, y_val, char_index = load_data_test()
+    params = {
+        'loss_function': 'binary_crossentropy',
+        'optimizer': 'adam',
+        'batch_train': 10,  # Reduced batch size for testing
+        'batch_test': 10,
+        'categories': ['phishing', 'legitimate'],
+        'epoch': 1,  # Reduced epochs for testing
+        'embedding_dimension': 50
+    }
+    model, history = train_model(x_train[:10], y_train[:10], x_val[:10], y_val[:10], params, char_index)
+    assert history.history['loss'][0] is not None, "Training did not produce a loss"
+    assert len(history.history['accuracy']) > 0, "Training did not produce accuracy metrics"
