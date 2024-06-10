39ad9e4e374cf20e870abf5b82d6512d762f1a0a
A.P. Ivanov <a.p.ivanov@student.tudelft.nl>
2024-06-10 23:20:52 +0200

    try fix flaky flake8?

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,8 +5,8 @@ from lib_ml.dataset import load_dataset_file
 
 @pytest.mark.parametrize("split", ("train", "val", "test"))
 def test_data(split):
-    params = dvc.api.params_show()
-    urls, labels = load_dataset_file(f"{params["dirs"]["dataset"]}/{split}.txt")
+    dataset = dvc.api.params_show()["dirs"]["dataset"]
+    urls, labels = load_dataset_file(f"{dataset}/{split}.txt")
 
     for url in urls:
         assert url, "URL should not be empty"
