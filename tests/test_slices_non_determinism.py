1a1de1a4bec626428879b94c9fca2d51f29c0b20
avilakathara <59567989+avilakathara@users.noreply.github.com>
2024-06-25 04:22:37 +0200

    Fix pylint issues

--- tests/test_slices_non_determinism.py
+++ tests/test_slices_non_determinism.py
@@ -1,6 +1,5 @@
+# pylint: disable=import-error
 import pytest
-import joblib
-from pathlib import Path
 from keras.models import load_model
 from numpy.testing import assert_almost_equal
 import dvc.api
@@ -12,14 +11,17 @@ from scripts.train import build_model, train_model, load_data
 model_path = 'outputs/train/model.keras'
 params = dvc.api.params_show()
 
+
 @pytest.fixture(scope="module")
 def data():
     params = dvc.api.params_show()
     return load_data(params)
 
+
 @pytest.fixture(scope="module")
 def trained_model():
     return load_model(model_path)
+
 
 def test_model_slices(data):
     x_train, y_train, x_val, y_val = data
@@ -31,13 +33,10 @@ def test_model_slices(data):
         end = low * 2
         model = build_model(params)
         train_model(model, params, x_train[low:end], y_train[low:end], validation_data=(x_val[low:end], y_val[low:end]))
-        loss, accuracy = model.evaluate(x_val, y_val)
+        _, accuracy = model.evaluate(x_val, y_val)
         results.append(accuracy)
 
-    mean_accuracy = sum(results) / len(results)
-
-    for acc in results:
-        assert len(results) == len(set(results)), "Not all accuracies are unique."
+    assert len(results) == len(set(results)), "Not all accuracies are unique."
 
 
 def test_model_non_determinism(data):
@@ -52,11 +51,10 @@ def test_model_non_determinism(data):
     for _ in range(2):
         model = build_model(params)
         train_model(model, params, x_train, y_train, validation_data=(x_val, y_val))
-        loss, accuracy = model.evaluate(x_val, y_val)
+        _, accuracy = model.evaluate(x_val, y_val)
         results.append(accuracy)
 
     mean_accuracy = sum(results) / len(results)
 
     for acc in results:
         assert_almost_equal(acc, mean_accuracy, decimal=2, err_msg="Model outputs vary too much")
-
