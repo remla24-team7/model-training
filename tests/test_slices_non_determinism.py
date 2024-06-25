5576bf7d2c9362d7c1d2f8ece8b15dcc4a089ea6
avilakathara <59567989+avilakathara@users.noreply.github.com>
2024-06-25 22:14:01 +0200

    Copied code into testing file to try and fix testing issues

--- tests/test_slices_non_determinism.py
+++ tests/test_slices_non_determinism.py
@@ -4,13 +4,14 @@ from keras.models import load_model
 from numpy.testing import assert_almost_equal
 import dvc.api
 import joblib
-
-from scripts.train import build_model, train_model
+from keras.src.models import Sequential
+from keras.src.layers import Embedding, Conv1D, MaxPooling1D, Dropout, Flatten, Dense
 
 model_path = 'outputs/train/model.keras'
 params = dvc.api.params_show()
 
 
+# Copied code for testing
 def load_data(params):
     preprocess_path = "outputs/preprocess/"
 
@@ -19,6 +20,65 @@ def load_data(params):
         joblib.load(preprocess_path + "y_train.joblib"),
         joblib.load(preprocess_path + "x_val.joblib"),
         joblib.load(preprocess_path + "y_val.joblib"),
+    )
+
+
+# Copied code for testing
+def build_model(params):
+    preprocess_path = "outputs/preprocess/"
+
+    tokenizer = joblib.load(preprocess_path / "tokenizer.joblib")
+    encoder = joblib.load(preprocess_path / "encoder.joblib")
+
+    input_dim = len(tokenizer.word_index.keys()) + 1
+    output_dim = len(encoder.classes_) - 1
+
+    return Sequential([
+        Embedding(input_dim, params["train"]["embedding_dim"]),
+
+        Conv1D(128, 3, activation="tanh"),
+        MaxPooling1D(3),
+        Dropout(0.2),
+
+        Conv1D(128, 7, activation="tanh", padding="same"),
+        Dropout(0.2),
+
+        Conv1D(128, 5, activation="tanh", padding="same"),
+        Dropout(0.2),
+
+        Conv1D(128, 3, activation="tanh", padding="same"),
+        MaxPooling1D(3),
+        Dropout(0.2),
+
+        Conv1D(128, 5, activation="tanh", padding="same"),
+        Dropout(0.2),
+
+        Conv1D(128, 3, activation="tanh", padding="same"),
+        MaxPooling1D(3),
+        Dropout(0.2),
+
+        Conv1D(128, 3, activation="tanh", padding="same"),
+        MaxPooling1D(3),
+        Dropout(0.2),
+
+        Flatten(),
+        Dense(output_dim, activation="sigmoid"),
+    ])
+
+
+# Copied code for testing
+def train_model(model, params, x_train, y_train, validation_data=None):
+    model.compile(
+        loss=params["train"]["loss_function"],
+        optimizer=params["train"]["optimizer"],
+        metrics=params["train"]["metrics"],
+    )
+
+    return model.fit(
+        x_train, y_train,
+        validation_data=validation_data,
+        batch_size=params["train"]["batch_size"],
+        epochs=params["train"]["epochs"],
     )
 
 
